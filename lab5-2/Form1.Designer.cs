7145a7fbfe7cff26cd04f04b06c58d87d0c551d6
TheSkyEye1 <golovan4ickoff@gmail.com>
2020-11-13 17:53:53 +0700

    1
    1

---
+++ lab5-2/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace lab5_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.IMG1 = new Emgu.CV.UI.ImageBox();
+            this.webon = new System.Windows.Forms.Button();
+            this.load = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.IMG1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // IMG1
+            // 
+            this.IMG1.Location = new System.Drawing.Point(12, 12);
+            this.IMG1.Name = "IMG1";
+            this.IMG1.Size = new System.Drawing.Size(600, 480);
+            this.IMG1.TabIndex = 2;
+            this.IMG1.TabStop = false;
+            // 
+            // webon
+            // 
+            this.webon.Location = new System.Drawing.Point(12, 498);
+            this.webon.Name = "webon";
+            this.webon.Size = new System.Drawing.Size(160, 29);
+            this.webon.TabIndex = 3;
+            this.webon.Text = "Web Camera";
+            this.webon.UseVisualStyleBackColor = true;
+            this.webon.Click += new System.EventHandler(this.webon_Click);
+            // 
+            // load
+            // 
+            this.load.Location = new System.Drawing.Point(178, 498);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(160, 29);
+            this.load.TabIndex = 4;
+            this.load.Text = "Load mask";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 533);
+            this.Controls.Add(this.load);
+            this.Controls.Add(this.webon);
+            this.Controls.Add(this.IMG1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.IMG1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox IMG1;
+        private System.Windows.Forms.Button webon;
+        private System.Windows.Forms.Button load;
+    }
+}
+
---
+++ lab5-2/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace lab5_2
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture capture;
+        Mat mask;
+        Image<Gray, byte> gray;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void webon_Click(object sender, EventArgs e)
+        {
+            capture = new VideoCapture();
+            capture.ImageGrabbed += ProcessFrame;
+            capture.Start();
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            var frame = new Mat();
+            capture.Retrieve(frame);
+            Image<Bgr, byte> image = frame.ToImage<Bgr, byte>();
+            Image<Gray, byte> grayImage = image.Convert<Gray, byte>();
+            using (CascadeClassifier cascadeClassifier = new CascadeClassifier(@"C:\haarcascade_frontalface_default.xml"))
+            {
+                Rectangle[] facesDetected = cascadeClassifier.DetectMultiScale(grayImage, 1.1, 10,
+                new Size(20, 20));
+                var copy = image.Copy();
+                    if (mask != null)
+                    {
+                        foreach (Rectangle rect in facesDetected)
+                        {
+                            Image<Bgra, byte> res = copy.Convert<Bgra, byte>();
+                            res.ROI = rect;
+                            Image<Bgra, byte> small = mask.ToImage<Bgra, byte>().Resize(rect.Width, rect.Height, Inter.Nearest);
+                            Image<Gray, byte> graymask = small.Convert<Gray, byte>();
+                            CvInvoke.cvCopy(small, res, small.Split()[3]);
+                            res.ROI = System.Drawing.Rectangle.Empty;
+                            IMG1.Image = res;
+                        }
+
+                    }
+                    else
+                    {
+                        foreach (Rectangle rect in facesDetected)
+                        {
+                            copy.Draw(rect, new Bgr(Color.Yellow), 2);
+                            IMG1.Image = copy;
+                        }
+                    }
+            }
+        }
+
+        private void load_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "(*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif;*.png";
+            var result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                mask = CvInvoke.Imread(fileName, ImreadModes.Unchanged);
+                gray = mask.ToImage<Gray, byte>();
+                IMG1.Image = gray;
+            }
+        }
+    }
+}
